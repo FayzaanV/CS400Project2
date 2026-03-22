895fd823c9d290345f3d7f4053794cd4c2806757
FayzaanV <fayzaanvirk6@yahoo.com>
2026-03-22 12:25:20 -0500

    Created Frontend class with temporary main() method

---
+++ Frontend.java
@@ -0,0 +1,26 @@
+public class Frontend implements FrontendInterface {
+    
+    public Frontend(BackendInterface backend) {
+
+    }
+
+    public String generateShortestPathPromptHTML() {
+        return null;
+    }
+
+    public String generateShortestPathResponseHTML(String start, String end) {
+        return null;
+    }
+
+    public String generateFurthestLocationListFromPromptHTML() {
+        return null;
+    }
+
+    public String generateFurthestLocationListFromResponseHTML(String start) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
