f294724631f21c7cf93c765e06b8892baa02c6dc
FayzaanV <fayzaanvirk6@yahoo.com>
2026-03-22 12:41:55 -0500

    Finished implementing the first method

--- Frontend.java
+++ Frontend.java
@@ -5,8 +5,10 @@ public class Frontend implements FrontendInterface {
     }
 
     public String generateShortestPathPromptHTML() {
-        String startInput = "<input type=>"
-        return null;
+        String startInput = "<input type='text' id='start' placeholder='Enter start location'>";
+        String endInput = "<input type='text' id='end' placeholder='Enter end location'>";
+        String button = "<button>Find Shortest Path</button>";
+        return startInput + "\n" + endInput + "\n" + button + "\n";
     }
 
     public String generateShortestPathResponseHTML(String start, String end) {
@@ -22,6 +24,9 @@ public class Frontend implements FrontendInterface {
     }
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        Graph_Placeholder graph = new Graph_Placeholder();
+        Backend_Placeholder backend = new Backend_Placeholder(graph);
+        Frontend frontend = new Frontend(backend);
+        System.out.println(frontend.generateShortestPathPromptHTML());
     }
 }
